72a57f9c265660cf04783d0628ac122ad65cf07f
faisalahmed0086 <faisalahmed0086@gmail.com>
2017-05-10 18:23:12 +0600

    Stalker Exercise

---
+++ js/stalker_ex.js
@@ -0,0 +1,7 @@
+var firstName=prompt("What is your firstName?");
+var lastName=prompt("What is your lastName?");
+var age=prompt("What is your age?");
+alert("So your name is " + firstName +" "+lastName);
+console.log("So your name is " + firstName +" "+lastName);
+alert("So you are " + age +" years old.");
+console.log("So you are " + age +" years old.");
