95a4d6fa70487ff8a276adf457f27f459dcf7a3c
faisalahmed0086 <faisalahmed0086@gmail.com>
2017-05-12 22:29:25 +0600

    While_ex

---
+++ js/while.js
@@ -0,0 +1,19 @@
+// console.log("Print all number between -10 & 19")
+
+// var num=-10;
+
+// while(num<=19){
+// 	console.log(num);
+// 	num++;
+// }
+
+console.log("Print all even numbers between 10 & 40")
+
+var num=5;
+
+while(num<=50){
+	if (num % 5==0 && num % 3 == 0) {
+		console.log(num);
+	}
+	 num++;
+}
