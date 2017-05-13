0e125c3fd9d19fd505f4d6367e2918411fde9f65
faisalahmed0086 <faisalahmed0086@gmail.com>
2017-05-13 13:34:10 +0600

    for_ex

---
+++ js/for_ex.js
@@ -0,0 +1,14 @@
+console.log("Printing all the even numbers between 10 & 40")
+
+for(var num=10;num <= 40;num++) {
+	if (num%2==0) {
+		console.log(num)
+	}
+	
+}
+
+// console.log("Printing all the number between -10 & 19")
+
+// for(var num=-10;num<20;num++) {
+// 	console.log(num)
+// }
